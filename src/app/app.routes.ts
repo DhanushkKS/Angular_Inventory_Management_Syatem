24d17fab6b03ba73883a1ba03072271a7460e020
DhanushkKS <ksdhanushka777@gmail.com>
2024-02-08 10:10:31 +0530

    Created navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { NewPurchaseComponent } from './pages/new-purchase/new-purchase.component';
+import { NewSaleComponent } from './pages/new-sale/new-sale.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PurchaseListComponent } from './pages/purchase-list/purchase-list.component';
+import { SaleListComponent } from './pages/sale-list/sale-list.component';
+import { StockComponent } from './pages/stock/stock.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent, title: 'Dashboard' },
+  {
+    path: 'new-purchase',
+    component: NewPurchaseComponent,
+    title: 'New Purchase',
+  },
+  { path: 'new-sale', component: NewSaleComponent, title: 'New Sale' },
+  {
+    path: 'purchase-list',
+    component: PurchaseListComponent,
+    title: 'Purchase List',
+  },
+  { path: 'sale-list', component: SaleListComponent, title: 'Sale List' },
+  { path: 'stock', component: StockComponent, title: 'Stock' },
+];
