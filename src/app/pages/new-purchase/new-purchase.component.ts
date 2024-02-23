bd1f67798cdf0ba6488bd0e93819175657703d32
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-02-23 20:22:06 +0530

    Fetched products fro api

--- src/app/pages/new-purchase/new-purchase.component.ts
+++ src/app/pages/new-purchase/new-purchase.component.ts
@@ -1,15 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForOf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  MatFormField,
+  MatFormFieldModule,
+  MatLabel,
+} from '@angular/material/form-field';
+import {
+  MatOption,
+  MatSelect,
+  MatSelectModule,
+} from '@angular/material/select';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { environment } from '../../../../environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-purchase',
   standalone: true,
-  imports: [NgForOf, FormsModule],
+  imports: [
+    NgForOf,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatButton,
+    MatGridList,
+    MatGridTile,
+  ],
   templateUrl: './new-purchase.component.html',
   styleUrl: './new-purchase.component.css',
 })
-export class NewPurchaseComponent {
+export class NewPurchaseComponent implements OnInit {
+  private url = `${environment.BASE_URL}/products`;
+  products: any[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProductsList();
+  }
+  getProductsList() {
+    this.http.get(this.url).subscribe({
+      next: (products: any) => {
+        this.products = products;
+        console.log(this.products);
+      },
+      error: (e) => {
+        console.log(e);
+      },
+      complete: () => {
+        console.log('completed');
+      },
+    });
+  }
+
+  purchaseForm = new FormGroup({
+    productId: new FormControl('', Validators.required),
+    quantity: new FormControl('', Validators.required),
+    supplierName: new FormControl('', Validators.required),
+    invoiceAmount: new FormControl('', Validators.required),
+    invoiceNumber: new FormControl('', Validators.required),
+  });
+
   purchaseBody: any = {
     purchaseDate: '2024-02-12',
     productId: 2,
@@ -33,4 +94,10 @@ export class NewPurchaseComponent {
     },
   ];
   productList: any = ([] = this.dummyProducts);
+  protected readonly onsubmit = onsubmit;
+  selectedProduct: number = 0;
+
+  onSubmit() {
+    console.log('submitted', this.selectedProduct);
+  }
 }
